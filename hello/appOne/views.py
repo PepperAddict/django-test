43fca1fc57518f510d45e10888da4e3ad3a661d5
unknown <jenearly@gmail.com>
2020-11-14 12:38:57 -0500

    test2

---
+++ hello/appOne/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "hello/index.html")
+
+
+def me(request, name):
+    return HttpResponse(f"HELLOOO, {name.capitalize()}!")
